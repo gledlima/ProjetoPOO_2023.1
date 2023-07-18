222a21554b058560c612923deea565094dd8e55d
leonardo-lbp <130298928+leonardo-lbp@users.noreply.github.com>
2023-07-17 22:00:06 -0300

    Update Comunidade.py

--- Comunidade.py
+++ Comunidade.py
@@ -1,39 +1,40 @@
-communtyList = []
+communityList = []
 
-def newCommunit(Cname, Cdescrition):
-    global communtyList
+def newCommunity(Cname, Cdescription):
+    global communityList
     Cmember = []
-    newCommunit = {'name': Cname, 'descrition': Cdescrition, 'member': Cmember}
-    communtyList.append(newCommunit)
+    newCommunity = {'name': Cname, 'description': Cdescription, 'member': Cmember}
+    communityList.append(newCommunity)
 
-def followCommunit(index, username, email):
-    global communtyList
+def followCommunity(index, username, email):
+    global communityList
     nemMember = {'username': username, 'email' : email}
-    communtyList[index]['member'].append(nemMember)
+    communityList[index]['member'].append(nemMember)
 
-def show(communtyNumber):
-    global communtyList
-    print('Account', communtyNumber)
-    thisAcommuntylist = communtyList[communtyNumber]
-    print('       name:', thisAcommuntylist['name'])
-    print('       decription:', thisAcommuntylist['descrition'])
-    print('       members:', len(thisAcommuntylist['member']))
+def show(communityNumber):
+    global communityList
+    print('Community', communityNumber)
+    thisAcommunityList = communityList[communityNumber]
+    print('       Name:', thisAcommunityList['name'])
+    print('       Decription:', thisAcommunityList['description'])
+    print('       Members:', len(thisAcommunityList['member']))
     print()
 
 def showMember(index1, index2):
-    global communtyList
-    members = communtyList[index1]['member']
-    print('       community name: ', communtyList[index1]['name'])
-    print('       member: ', members[index2]['username'])
-    print('       email: ', members[index2]['email'])
+    global communityList
+    members = communityList[index1]['member']
+    print('       Community Name: ', communityList[index1]['name'])
+    print('       Member: ', members[index2]['username'])
+    print('       Email: ', members[index2]['email'])
     print()
 
 while True:
     print()
     print('Pressione a para criar uma comunidade')
-    print('Pressione b para mostrar todas as comunidades')
-    print('Pressione c para seguir uma comunidade')
-    print('Pressione d para mostrar seguidores de uma comunidade')
+    print('Pressione b para excluir uma comunidade')
+    print('Pressione c para mostrar todas as comunidades')
+    print('Pressione d para seguir uma comunidade')
+    print('Pressione e para mostrar seguidores de uma comunidade')
     print('Pressione x para sair')
     print()
 
@@ -45,41 +46,40 @@ while True:
     if action == 'a':
         print('Criação de comunidade:')
         cname = input('Insira o nome da comunidade: ')
-        cdescrition = input('insira a descricao da comunidade: ')
+        cdescription = input('insira a descricao da comunidade: ')
         cmember = []
-        newCommunit(cname, cdescrition)
+        newCommunity(cname, cdescription)
 
-    elif action == 'b':
+    elif action == 'c':
         print('Lista de Contas:')
-        ncommunit = len(communtyList)
+        ncommunit = len(communityList)
         for acomunitNumber in range(0, ncommunit):
             show(acomunitNumber)
 
-    elif action == 'c':
+    elif action == 'd':
         print('Seguir comunidade:')
         name = input('Insira o nome da comunidade que deseja seguir: ')
         index = 0
-        for a in communtyList:
+        for a in communityList:
             if a['name'] == name:
                 name = input('Insira seu nome: ')
                 email = input('Insira seu email ')
-                followCommunit(index, name, email)
+                followCommunity(index, name, email)
                 break
             index+=1
 
-    elif action == 'd':
+    elif action == 'e':
         print('Mostrar membros:')
         name = input('Insira o nome da comunidade que deseja exibir membros: ')
         index = 0
-        for a in communtyList:
+        for a in communityList:
             if a['name'] == name:
-                members = len(a['member'])
-                for b in range(0, members):
+                nummembers = len(a['member'])
+                for b in range(0, nummembers):
                     showMember(index, b)
                 break 
             index+=1
                 
-
     elif action == 'x':
         break    
 
