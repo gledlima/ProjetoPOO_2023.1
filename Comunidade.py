0115a83c459be41d9802f0d27af4343fedae3e8c
leonardo-lbp <130298928+leonardo-lbp@users.noreply.github.com>
2023-07-18 14:54:50 -0300

    Update Comunidade.py

--- Comunidade.py
+++ Comunidade.py
@@ -1,4 +1,7 @@
 import json
+import time
+import main
+import sys
 communityList = []
 accountsList = []
 
@@ -33,10 +36,7 @@ def show(communityNumber):
 def showMember(index1, index2):
     global communityList
     members = communityList[index1]['member']
-    print('       Community Name: ', communityList[index1]['name'])
-    print('       Member: ', members[index2]['username'])
-    print('       Email: ', members[index2]['email'])
-    print()
+    print('       Member','(',index2,')',':', members[index2]['username'])
 
 
 
@@ -57,7 +57,7 @@ def followAccount(index, name):
 
 
 #Bloco de funções de manipulação do txt
-def saveChanges():
+def saveChangesCommunity():
     with open("Comunidade.txt", "w") as file:
         for a in communityList:
             file.write("%s\n%s\n" % (a['name'], a['description']))
@@ -107,117 +107,186 @@ with open("Comunidade.txt", "r") as file:
 
 
 
-
 #Função de operações
-while True:
-    print()
-    print('Pressione a para criar uma comunidade')
-    print('Pressione b para excluir uma comunidade')
-    print('Pressione c para mostrar todas as comunidades')
-    print('Pressione d para seguir uma comunidade')
-    print('Pressione e para deixar de seguir uma comunidade')
-    print('Pressione f para mostrar seguidores de uma comunidade')
-    print('Pressione x para sair')
-    print()
+def operations():
+    while True:
+        print()
+        print()
+        print('Pressione A para criar uma comunidade')
+        print('Pressione B para excluir uma comunidade')
+        print('Pressione C para seguir uma comunidade')
+        print('Pressione D para deixar de seguir uma comunidade')
+        print('Pressione E para mostrar todas as comunidades')
+        print('Pressione F para mostrar seguidores de uma comunidade')
+        print('Pressione G para editar uma comunidade')
+        print('Pressione Q para retornar ao menu')
+        print('Pressione X para retornar ao menu')
+        print()
 
-    action = input('Oque você quer fazer? ')
-    action = action.lower()
-    action = action[0]
-    print()
-
-
-    if action == 'a':
-        print('Criação de comunidade:')
-        cname = input('Insira o nome da comunidade: ')
-        for community in communityList:
-            if community['name'] == cname:
-                print('Esse nome de comunidade já está em uso. Por favor, escolha outro nome.')
-                saveChanges()
-                break
-        else:
-            cdescription = input('Insira a descrição da comunidade: ')
-            cmember = []
-            newCommunity(cname, cdescription)
+        action = input('Oque você quer fazer? ')
+        action = action.lower()
+        action = action[0]
+        print()
 
 
-    elif action == 'b':
-        print('Exclusão de comunidade:')
-        communityName = input('Insira o nome da comunidade que deseja excluir: ')
-        community_found = False
-
-        for community in communityList:
-            if community['name'] == communityName:
-                communityList.remove(community)
-                print('Comunidade excluída com sucesso!')
-                community_found = True
-                saveChanges()
-                break
-
-        if not community_found:
-            print('A comunidade inserida é inválida ou não existe.')
+        if action == 'a':
+            time.sleep(0.5)
+            print('Criação de comunidade:')
+            cname = input('Insira o nome da comunidade: ')
+            for community in communityList:
+                if community['name'] == cname:
+                    print('Esse nome de comunidade já está em uso. Por favor, escolha outro nome.')
+                    break
+            else:
+                cdescription = input('Insira a descrição da comunidade: ')
+                cmember = []
+                newCommunity(cname, cdescription)
 
 
-    elif action == 'c':
-        print('Lista de comunidades:')
-        nCommunity = len(communityList)
-        for aCommunityNumber in range(0, nCommunity):
-            show(aCommunityNumber)
+        elif action == 'b':
+            time.sleep(0.5)
+            print('Exclusão de comunidade:')
+            communityName = input('Insira o nome da comunidade que deseja excluir: ')
+            community_found = False
+
+            for community in communityList:
+                if community['name'] == communityName:
+                    provLogin = input('Insira o login: ')
+                    provPassword = input('Insira a senha: ')
+                    for b in accountsList:
+                        if b['login'] == provLogin and b['password'] == provPassword:
+                            provName = b['name']
+                            if len(community['member']) == 0 or provName == community['member'][0]['username']:
+                                communityList.remove(community)
+                                print('Comunidade excluída com sucesso!')
+                                community_found = True
+                                break
+                            else:
+                                print('Você não tem permissão para excluir esta comunidade.')
+                    else:
+                        print('Credenciais inválidas, tente novamente')
+                    break
+            else:
+                if not community_found:
+                    print('A comunidade inserida é inválida ou não existe.')
 
 
-    elif action == 'd':
-        print('Seguir comunidade:')
-        name = input('Insira o nome da comunidade que deseja seguir: ')
-        index = 0
-        for a in communityList:
-            if a['name'] == name:
-                name = input('Insira seu nome: ')
-                followCommunity(index, name)
-                saveChanges()
-                break
-            index+=1
-        else:
-            print('Nome inválido, tente novamente')
+        elif action == 'c':
+            time.sleep(0.5)
+            print('Seguir comunidade:')
+            name = input('Insira o nome da comunidade que deseja seguir: ')
+            index = 0
+            for a in communityList:
+                if a['name'] == name:
+                    provLogin = input('Insira o login: ')
+                    provPassword = input('Insira a senha: ')
+                    for b in accountsList:
+                        if b['login'] == provLogin and b['password'] == provPassword:
+                            followCommunity(index, b['name'])
+                            print('Comunidade seguida com sucesso!')
+                    break
+                index+=1
+            else:
+                print('Nome inválido, tente novamente')
 
 
-    elif action == 'e':
-        print('Deixar de seguir uma comunidade:')
-        communityName = input('Insira o nome da comunidade que deseja deixar de seguir: ')
-        account_found = False
+        elif action == 'd':
+            time.sleep(0.5)
+            print('Deixar de seguir uma comunidade:')
+            communityName = input('Insira o nome da comunidade que deseja deixar de seguir: ')
+            account_found = False
+            community_found = False
 
-        for community in communityList:
-            if community['name'] == communityName:
-                username = input('Insira seu nome: ')
-                email = input('Insira seu email: ')
-                for member in community['member']:
-                    if member['username'] == username and member['email'] == email:
-                        community['member'].remove(member)
-                        print('Deixou de seguir a comunidade com sucesso!')
-                        account_found = True
-                        saveChanges()
-                        break
-                else:
-                    print('Não foi possível encontrar suas credenciais de membro na comunidade.')
-                break
+            for community in communityList:
+                if community['name'] == communityName:
+                    community_found = True
+                    provLogin = input('Insira o login: ')
+                    provPassword = input('Insira a senha: ')
+                    for b in accountsList:
+                        if b['login'] == provLogin and b['password'] == provPassword:
+                            for member in community['member']:
+                                if member['username'] == b['name']:
+                                    community['member'].remove(member)
+                                    print('Deixou de seguir a comunidade com sucesso!')
+                                    account_found = True
+                                    break
 
-        if not account_found:
-            print('A comunidade inserida é inválida ou você não é um membro dela.')
+            if not community_found:
+                print('O nome da comunidade inserida é inválido, tente novamente')
+            elif not account_found:
+                print('Você não é um membro desta comunidade ou suas credenciais são inválidas.')
+
+        
+        elif action == 'e':
+            time.sleep(0.5)
+            print('Lista de comunidades:')
+            nCommunity = len(communityList)
+            for aCommunityNumber in range(0, nCommunity):
+                show(aCommunityNumber)
 
 
-    elif action == 'f':
-        print('Mostrar membros:')
-        name = input('Insira o nome da comunidade que deseja exibir membros: ')
-        index = 0
-        for a in communityList:
-            if a['name'] == name:
-                nummembers = len(a['member'])
-                for b in range(0, nummembers):
-                    showMember(index, b)
-                break 
-            index+=1
-                
+        elif action == 'f':
+            time.sleep(0.5)
+            print('Mostrar membros:')
+            name = input('Insira o nome da comunidade que deseja exibir membros: ')
+            index = 0
+            print()
+            print('Community Name: ', name)
+            for a in communityList:
+                if a['name'] == name:
+                    nummembers = len(a['member'])
+                    for b in range(0, nummembers):
+                        showMember(index, b)
+                    break 
+                index+=1
 
-    elif action == 'x':
-        saveChanges()
-        break    
+        
+        elif action == 'g':
+            time.sleep(0.5)
+            print('Editar comunidade:')
+            communityName = input('Insira o nome da comunidade que deseja editar: ')
+            community_found = False
+
+            for community in communityList:
+                if community['name'] == communityName:
+                    community_found = True
+                    provLogin = input('Insira o login: ')
+                    provPassword = input('Insira a senha: ')
+                    for b in accountsList:
+                        if b['login'] == provLogin and b['password'] == provPassword:
+                            provName = b['name']
+                            if len(community['member']) == 0 or provName == community['member'][0]['username']:
+                                newCname = input('Insira o novo nome da comunidade: ')
+                                newCdescription = input('Insira a nova descrição da comunidade: ')
+                                community['name'] = newCname
+                                community['description'] = newCdescription
+                                print('Comunidade editada com sucesso!')
+                            else:
+                                print('Você não tem permissão para editar esta comunidade.')
+                            break
+                    else:
+                        print('Credenciais inválidas, tente novamente')
+                    break
+
+            if not community_found:
+                print('A comunidade inserida é inválida ou não existe.')
+
+
+        elif action == 'q':
+            main.menu()
+
+
+        elif action == 'x':
+            print('Programa fechado!')
+            time.sleep(0.5)
+            sys.exit()
+
+
+        time.sleep(0.5)
+        saveChangesCommunity()
 
 print('Done')
+
+
+if __name__ == '__main__':
+    operations()
