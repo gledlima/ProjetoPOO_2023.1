83f3b35c0baca99aa02a4f31293c9888df29745a
leonardo-lbp <130298928+leonardo-lbp@users.noreply.github.com>
2023-07-18 05:15:25 -0300

    Update Conta.py

--- Conta.py
+++ Conta.py
@@ -1,13 +1,36 @@
 import json
+communityList = []
 accountsList = []
+
+
+
+
+#Bloco de funções de manipulação da comunidade
+def newCommunity(Cname, Cdescription):
+    global communityList
+    Cmember = []
+    newCommunity = {'name': Cname, 'description': Cdescription, 'member': Cmember}
+    communityList.append(newCommunity)
+
+def followCommunity(index, username, email):
+    global communityList
+    newMember = {'username': username, 'email' : email}
+    communityList[index]['member'].append(newMember)
+
 
 
 
 #Bloco de funções de manipulação da conta
 def newAccount(aLogin, aPassword, aName, aEmail):
     global accountsList
-    newAccountDict = {'login': aLogin, 'password': aPassword, 'name': aName, 'email': aEmail}
+    aFollower = []
+    newAccountDict = {'login': aLogin, 'password': aPassword, 'name': aName, 'email': aEmail, 'follower': aFollower}
     accountsList.append(newAccountDict)
+
+def followAccount(index, name):
+    global accountsList
+    newMember = {'name': name}
+    accountsList[index]['follower'].append(newMember)
 
 def editAccount(accountNumber, field, newValue):
     global accountsList
@@ -21,7 +44,9 @@ def show(accountNumber):
     print('       Password:', thisAccountDict['password'])
     print('       Name:', thisAccountDict['name'])
     print('       Email:', thisAccountDict['email'])
+    print('       Followers:', len(thisAccountDict['follower']))
     print()
+
 
 
 
@@ -30,8 +55,14 @@ def saveChanges():
     with open("Conta.txt", "w") as file:
         for a in accountsList:
             file.write("%s %s %s %s\n" % (a['login'], a['password'], a['name'], a['email']))
+            numFollowers = len(a['follower'])
+            file.write("%i\n" % numFollowers)
+            for b in a['follower']:
+                file.write(b['name'])
+                file.write("\n")
 
 with open("Conta.txt", "r") as file:
+    index = 0
     for user in file:
         u = user.split()
         login = u[0]
@@ -39,8 +70,17 @@ with open("Conta.txt", "r") as file:
         name = u[2]
         email = u[3]
         newAccount(login, password, name, email)
-    file.close()
 
+        next_line = next(file)
+        num_followers = int(next_line.strip())
+
+        for follower in range(num_followers):
+            next_line = next(file)
+            account = next_line.strip()
+            followAccount(index, account)
+        index += 1 
+
+            
 
 
 #Função de operações
@@ -49,7 +89,9 @@ while True:
     print('Pressione a para criar uma conta')
     print('Pressione b para excluir uma conta')
     print('Pressione c para mostrar todas as contas')
-    print('Pressione d para editar uma conta')
+    print('Pressione d para seguir uma conta')
+    print('Pressione e para deixar de seguir uma conta')
+    print('Pressione f para editar uma conta')
     print('Pressione x para sair')
     print()
 
@@ -57,6 +99,7 @@ while True:
     action = action.lower()
     action = action[0]
     print()
+
 
     if action == 'a':
         print('Nova conta:')
@@ -83,18 +126,25 @@ while True:
                     saveChanges()
                     print('Conta criada com sucesso!')
 
+
     elif action == 'b':
         print('Exclusão de conta:')
         provLogin = input('Qual seu login? ')
-        userPassword = input('Qual sua senha? ')
+        provPassword = input('Qual sua senha? ')
+        removed_accounts = []
         for a in accountsList:
-            if a['login'] == provLogin and a['password'] == userPassword:
+            if a['login'] == provLogin and a['password'] == provPassword:
                 accountsList.remove(a)
+                removed_accounts.append(a['name'])
                 print('Conta excluída com sucesso!')
-                saveChanges()
-                break
+        if removed_accounts:
+            for a in accountsList:
+                followers = a['follower']
+                a['follower'] = [follower for follower in followers if follower['name'] not in removed_accounts]
         else:
             print('Credenciais inválidas. Não foi possível excluir a conta.')
+        saveChanges()
+
 
     elif action == 'c':
         print('Lista de Contas:')
@@ -102,7 +152,68 @@ while True:
         for accountNumber in range(nAccounts):
             show(accountNumber)
 
+
     elif action == 'd':
+        print('Seguir conta:')
+        provName = input('Insira o username da conta que deseja seguir: ')
+        index = 0
+        account_found = False
+
+        for a in accountsList:
+            if a['name'] == provName:
+                provLogin = input('Insira seu Login: ')
+                provPass = input('Insira sua Senha: ')
+                for b in accountsList:
+                    if b['login'] == provLogin and b['password'] == provPass:
+                        follower_names = [follower['name'] for follower in a['follower']]
+                        if b['name'] in follower_names:
+                            print('Você já está seguindo essa conta.')
+                        else:
+                            a['follower'].append({'name': b['name']})
+                            print('Seguido com sucesso!')
+                        account_found = True
+                        break
+                else:
+                    print('Credenciais inválidas, tente novamente')
+                break
+
+            index += 1
+
+        if not account_found:
+            print('O username inserido é inválido, tente novamente')
+
+
+    elif action == 'e':
+        print('Deixar de seguir uma conta:')
+        provName = input('Insira o username da conta que deseja deixar de seguir: ')
+        index = 0
+        account_found = False
+
+        for a in accountsList:
+            if a['name'] == provName:
+                provLogin = input('Insira seu Login: ')
+                provPass = input('Insira sua Senha: ')
+                for b in accountsList:
+                    if b['login'] == provLogin and b['password'] == provPass:
+                        follower_names = [follower['name'] for follower in a['follower']]
+                        if b['name'] not in follower_names:
+                            print('Você não está seguindo essa conta.')
+                        else:
+                            a['follower'] = [follower for follower in a['follower'] if follower['name'] != b['name']]
+                            print('Deixou de seguir com sucesso!')
+                        account_found = True
+                        break
+                else:
+                    print('Credenciais inválidas, tente novamente')
+                break
+
+            index += 1
+
+        if not account_found:
+            print('O username inserido é inválido, tente novamente')
+
+
+    elif action == 'f':
         print('Edição de conta:')
         provLogin = input('Qual seu login? ')
         userPassword = input('Qual sua senha? ')
@@ -156,6 +267,7 @@ while True:
         else:
             print('Credenciais inválidas. Não foi possível editar a conta.')       
         
+
     elif action == 'x':
         print('Programa fechado')
         saveChanges()
